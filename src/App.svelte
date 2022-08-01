a2c8d4f3f81dd64a9ccc47e7d2a7b341b5c8d070
Pau Fernández <pau.fernandez@upc.edu>
2022-08-01 11:02:45 +0200

    Planning

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,32 @@
   import { onDestroy } from "svelte";
   import ErrorMessage from "./lib/ErrorMessage.svelte";
 
+  /*
+  Connection:
+  - Crear una conexión (esto debería estar todo el rato): connectToSerialPort
+  - Poner un for await a leer datos
+  - Pausar la conexión (parar el proceso que lee datos).
+  - Reanudar la conexión (volver a empezar el proceso que lee datos).
+  - Cerrar la conexión.
+
+  Los stores son *GRANULARES*, haces un store para cada cosa que el UI necesita!
+  > Samples puede ser un store. (readable, easy)
+  > ConnectionState puede ser otro store (readable)
+  > Errors puede ser otro (readable)
+  > Un writable para indicar qué hay que hacer?
+
+  $samples
+  $connectionState
+  $errors
+
+  funciones:
+  - connectToSerialPort
+  - pause
+  - resume
+  - disconnect
+
+  */
+
   import { Connection, connectToSerialPort } from "./lib/serial";
   import Table from "./lib/Table.svelte";
   import type { Sample } from "./lib/types";
